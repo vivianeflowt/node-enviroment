35ed33f79f77b100bc8b51f83e54bc0843181377
vivianeflowt <vivianeflowt@gmail.com>
2020-11-19 13:09:29 -0300

    kkk

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,37 @@
-const { watch, series } = require("gulp");
+const gulp = require("gulp");
 const shell = require("shelljs");
 
-function defaultTask(cb) {
-  // place code for your default task here
-  console.log("gulp...");
-  cb();
-}
-
-function clean(cb) {
-  // body omitted
-  cb();
-}
-
-function javascript(cb) {
-  // body omitted
-  console.log("gulp js...");
-  cb();
-}
-
-function css(cb) {
-  // body omitted
-  console.log("gulp css...");
-  cb();
-}
-
-exports.default = function () {
-  //@ browserify
-  watch(["src/*.js"], function (cb) {
-    // body omitted
-    console.log("gulp bundle...");
-    // shell.exec(
-    //   "browserify ./src/index.js -t babelify --outfile ./public/bundle.js"
-    // );
-    cb();
-  });
+const config = {
+  browserify: true,
 };
+
+gulp.task("default", function (done) {
+  console.log("default");
+  done();
+});
+
+gulp.task("clean", function () {
+  console.log("clean");
+});
+
+// Message
+gulp.task("message", function () {
+  console.log("Gulp is running...");
+});
+
+// Auto Generate Browserify Bundle
+gulp.task("browserify", function (done) {
+  watch(["src/*.js"], function (cb) {
+    if (config.browserify) {
+      console.log("browserify bundle...");
+      shell.exec(
+        "browserify ./src/index.js -t babelify --outfile ./public/bundle.js"
+      );
+    }
+  });
+  done();
+});
+
+// exports.default = function () {
+//   console.log("Gulp is running...");
+// };
