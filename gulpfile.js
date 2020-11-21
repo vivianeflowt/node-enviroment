abd7ce408a0ae5027b3bb807368852c103079781
vivianeflowt <vivianeflowt@gmail.com>
2020-11-21 03:24:56 -0300

    tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,67 @@
 const gulp = require("gulp");
-const shell = require("shelljs");
 const clean = require("gulp-clean");
+const cleanCSS = require("gulp-clean-css");
+const run = require("gulp-run");
+const rename = require("gulp-rename");
+const concat = require("gulp-concat");
+const babel = require("gulp-babel");
+const watch = require("gulp-watch");
+const browserify = require("gulp-browserify");
+const browserSync = require("browser-sync");
+
+const buildBundle = (done) => {
+  run(
+    "browserify ./src/index.js -t babelify --outfile ./build/bundle.js"
+  ).exec();
+  done();
+};
+
+const buildApp = (done) => {
+  run("npx babel ./src --out-dir ./build").exec();
+  done();
+};
 
 gulp.task("default", function (done) {
   console.log("default");
   done();
 });
 
-gulp.task("clean", function () {
-  gulp.console.log("clean");
-});
+gulp.task("browserify", buildBundle);
 
-// Message
-gulp.task("message", function () {
-  console.log("Gulp is running...");
-});
-
-// Auto Generate Browserify Bundle
-gulp.task("browserify", function (done) {
-  watch(["src/*.js"], function (cb) {
-    if (config.browserify) {
-      console.log("browserify bundle...");
-      shell.exec(
-        "browserify ./src/index.js -t babelify --outfile ./public/bundle.js"
-      );
-    }
-  });
+gulp.task("browserify-watch", (done) => {
+  gulp.watch("./src/index.js", buildBundle);
   done();
 });
 
-gulp.task("default", function () {
-  return gulp.src("build/*", { read: false }).pipe(clean());
+gulp.task("babel", buildApp);
+
+gulp.task("clean", (done) => {
+  gulp.src("build/*.js", { read: false }).pipe(clean({ force: true }));
+  gulp.src("build/*.css", { read: false }).pipe(clean({ force: true }));
+  done();
 });
 
-// exports.default = function () {
-//   console.log("Gulp is running...");
-// };
+gulp.task("concat", function (done) {
+  gulp.src("./src/*.js").pipe(concat("all.js")).pipe(gulp.dest("./build"));
+  done();
+});
+
+gulp.task("css", (done) => {
+  gulp
+    .src("styles/*.css")
+    .pipe(
+      cleanCSS({ debug: true }, (details) => {
+        console.log(`${details.name}: ${details.stats.originalSize}`);
+        console.log(`${details.name}: ${details.stats.minifiedSize}`);
+      })
+    )
+    .pipe(gulp.dest("build/"));
+  done();
+});
+
+exports.default = (done) => {
+  console.log("Gulp is running...");
+  gulp.src("build/*.js", { read: false }).pipe(clean({ force: true }));
+  gulp.src("build/*.css", { read: false }).pipe(clean({ force: true }));
+  done();
+};
