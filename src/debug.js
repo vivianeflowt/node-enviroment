ea3f048624407edaaac633377ca9f7ce19e810d2
vivianeflowt <vivianeflowt@gmail.com>
2020-11-29 05:31:40 -0300

    init

---
+++ src/debug.js
@@ -0,0 +1,19 @@
+let counter = 0;
+
+setTimeout(() => {
+  try {
+    document.body.innerHTML += "<p> console debug... </p>";
+    setInterval(() => {
+      try {
+        console.log(`[${counter}] Debug...`);
+        counter++;
+      } catch (error) {
+        console.log(error);
+      }
+    }, 5000);
+  } catch (error) {
+    console.log(error);
+  }
+}, 3000);
+
+//
